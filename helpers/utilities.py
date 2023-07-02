8b9122cda3a2dde1aca08bbb2e0c2ac2bda37130
ggurjar333 <ggurjar333@gmail.com>
2023-07-02 09:42:46 +0530

    s3helpers is updated.

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -8,9 +8,10 @@ class Utility:
 
     def create_tmp_folder(self, folder_name):
         self._name = folder_name
-
-        if not pathlib.Path(self._name):
-            return pathlib.Path(self._name).mkdir(parents=True, exist_ok=True)
+        p = pathlib.Path(self._name)
+        p.mkdir(parents=True, exist_ok=True)
+        # if not pathlib.Path(self._name):
+            # return pathlib.Path(self._name).mkdir(parents=True, exist_ok=True)
 
     def __str__(self) -> str:
         return str(self._name)
